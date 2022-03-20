17d64c10041e698f97d72fd724e4fabe18598b3f
alvingohhm <alvgoh@yahoo.com>
2022-03-21 03:10:31 +0800

    create estatelist and estate component

---
+++ components/Estate/Estate.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="col mb-4" :id="id">
+    <div class="card shadow-sm">
+      <img :src="image" class="card-img-top" alt="house1" />
+
+      <div class="card-body">
+        <h5 class="card-title">{{ property_name }}</h5>
+        <div class="d-flex justify-content-between">
+          <h6>{{ currency }} {{ formatedPrice }}</h6>
+          <p>
+            {{ formatedArea }}, {{ formatedBedroom }}, {{ formatedBathroom }}
+          </p>
+        </div>
+        <div class="my-2">
+          <div>{{ location }}</div>
+          <div>{{ country }}</div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <h6 class="mb-2">Add Service to Cart</h6>
+        <div class="d-flex justify-content-start">
+          <button type="button" class="btn btn-primary me-2">
+            Agent Visit
+          </button>
+          <button type="button" class="btn btn-primary">Agent Call</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import millify from "millify";
+export default {
+  name: "Estate",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    property_name: {
+      type: String,
+      require: true,
+    },
+    currency: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: String,
+      require: true,
+    },
+    area: {
+      type: String,
+      require: true,
+    },
+    bedroom: {
+      type: String,
+      require: true,
+    },
+    bathroom: {
+      type: String,
+      require: true,
+    },
+    location: {
+      type: String,
+      require: true,
+    },
+    country: {
+      type: String,
+      require: true,
+    },
+  },
+  computed: {
+    formatedPrice: function () {
+      return millify(parseInt(this.price), {
+        precision: 3,
+        lowercase: true,
+      });
+    },
+    formatedArea: function () {
+      return this.area + " sqft";
+    },
+    formatedBedroom: function () {
+      return this.bedroom + (this.bedroom > 1 ? " Beds" : " Bed");
+    },
+    formatedBathroom: function () {
+      return this.bathroom + (this.bathroom > 1 ? " Baths" : " Bath");
+    },
+  },
+};
+</script>
+
+<style scoped>
+p,
+h6 {
+  margin-bottom: 0px;
+}
+</style>
---
+++ components/Estate/EstateLists.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-3">
+    <Estate
+      v-for="estate in listings"
+      :key="estate.id"
+      :id="estate.id"
+      :image="estate.image"
+      :property_name="estate.property_name"
+      :currency="estate.currency"
+      :price="estate.price"
+      :area="estate.area"
+      :bedroom="estate.bedrm"
+      :bathroom="estate.bathrm"
+      :location="estate.location"
+      :country="estate.country"
+    />
+  </div>
+</template>
+
+<script>
+import Estate from "@/components/Estate/Estate.vue";
+export default {
+  name: "EstateLists",
+  components: {
+    Estate,
+  },
+  props: {
+    listings: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
