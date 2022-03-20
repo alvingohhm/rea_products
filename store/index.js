aa21324bd1327363570708876936afe0eb89d9e4
alvingohhm <alvgoh@yahoo.com>
2022-03-21 03:11:50 +0800

    create store for state managment

---
+++ store/index.js
@@ -0,0 +1,38 @@
+import Vuex from "vuex";
+import axios from "axios";
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      listings: [],
+      cartCount: 0,
+      cart: [],
+    },
+    mutations: {
+      setListings(state, allListings) {
+        state.listings = allListings;
+      },
+    },
+    actions: {
+      nuxtServerInit(vuexContext, context) {
+        return axios
+          .get(`${process.env.BASEURL}/listings.json`)
+          .then((res) => {
+            const listingsArray = [];
+            for (const key in res.data) {
+              listingsArray.push({ ...res.data[key], id: key });
+            }
+            vuexContext.commit("setListings", listingsArray);
+          })
+          .catch((e) => context.error(e));
+      },
+    },
+    getters: {
+      getListings(state) {
+        return state.listings;
+      },
+    },
+  });
+};
+
+export default createStore;
