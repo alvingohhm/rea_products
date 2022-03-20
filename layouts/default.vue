25e5e8a85066c26aa62a9cdca3c2c2ba67833a62
alvingohhm <alvgoh@yahoo.com>
2022-03-20 21:27:27 +0800

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+    <main class="container d-flex">
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  margin-top: 80px;
+}
+</style>
